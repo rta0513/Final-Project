45e615f71ddd7b0b5f31432749c99fcbc0d5b259
Ryan <Ryan@email.com>
2024-04-07 15:03:11 -0400

    Added comments to imports

--- pyscan.py
+++ pyscan.py
@@ -1,9 +1,12 @@
 # Imports modules needed to run script
+# Used to choose random ASCII artwork
 import random
+# Used to exit program
 import sys
+# Used to run external commands
 import subprocess
+# Used to obtain system paths
 import os
-
 
 # Defines art display function
 def art_dis():
