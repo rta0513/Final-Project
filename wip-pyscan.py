c654300b8f3e4b23efaa76879fd71c525394b6f6
Ryan <Ryan@email.com>
2024-04-09 18:22:51 -0400

    Created WIP Script

---
+++ wip-pyscan.py
@@ -0,0 +1,14 @@
+# Imports modules needed to run script
+# Used to choose random ASCII artwork
+import random
+# Used to exit program
+import sys
+# Used to run external commands
+import subprocess
+# Used to obtain system paths
+import os
+
+def welcome_handler():
+    pass
+def configuration_handler():
+    pass
